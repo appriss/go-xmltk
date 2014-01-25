7ad15176cb9e2494d8c3074eccbd1ee2596c91f2
Donavan Pantke <dpantke@dpantke-rmac.att.net>
2014-01-25 02:03:29 -0500

    Refactored test to allow for easier test case writing.

--- canonical_test.go
+++ canonical_test.go
@@ -8,11 +8,20 @@ import "fmt"
 
 //Tests currently call "xmllint" in a dummy function. Replace function with real implementation.
 
-func testCompareResult(file string, sample []byte ) (bool, error) {
-    target, err := ioutil.ReadFile(file)
-    if err != nil {return false, err}
-    result := bytes.Equal(sample, target)
-    return result, nil
+func testXMLCompare(infile string, goldfile string) error {
+   indoc , err := ioutil.ReadFile(infile)
+   if err != nil { return err }
+   golddoc, err := ioutil.ReadFile(goldfile)
+   if err != nil { return err }
+   // Here's the function, we may refactor
+   outdoc, err := Canonicalize(indoc, XML_C14N_10, true)
+   if err != nil { return err }
+   exact := bytes.Equal(outdoc, golddoc)
+   if exact != true {
+   	err := fmt.Errorf("Output doc %s does not match success file %q", outdoc, goldfile)
+   	return err
+   }
+   return nil
 }
 
 
@@ -24,15 +33,20 @@ func testCompareResult(file string, sample []byte ) (bool, error) {
 //Normalization of whitespace outside of document.
 //Loss of whitespace between PI Target and its data.
 //Retention of whitespace inside PI data.
-
 func TestCanonical10Step31 (t *testing.T) {
-   indoc , err := ioutil.ReadFile("testdata/test-canonical1.0-input-3.1.xml")
-   if err != nil { t.Error(err.Error()); return }
-   // Here's the function, we may refactor
-   outdoc, err := Canonicalize(indoc, XML_C14N_10, true)
-   if err != nil { t.Error(err.Error()); return }
-   result, err := testCompareResult("testdata/test-canonical1.0-output-3.1.xml", outdoc)
-   if err != nil { t.Error(err.Error()); return }
-   if result != true { fmt.Printf("%s",outdoc)
-      t.Error("Output does not match expected result, file test-canonical1.0-input-3.1.xml") }
+   err := testXMLCompare("testdata/test-canonical1.0-input-3.1.xml","testdata/test-canonical1.0-output-3.1.xml")
+   if err != nil {
+     t.Error(err.Error())
+     return
+   }
+}
+
+//Test 2: http://www.w3.org/TR/xml-c14n#Example-WhitespaceInContent
+//Function must demonstrate the following:
+//Retain all whitespace between consecutive start tags, clean or dirty
+//Retain all whitespace between consecutive end tags, clean or dirty
+//Retain all whitespace between end tag/start tag pair, clean or dirty
+//Retain all whitespace in character content, clean or dirty
+func TestCanonical10Step32 (t *testing.T) {
+
 }
