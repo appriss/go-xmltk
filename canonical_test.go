0e5c042ff293163b98a0f553cc34f96542137a7a
Donavan Pantke <dpantke@dpantke-rmac.local>
2014-01-26 11:39:08 -0500

    Added remaining tests for Canonical XML 1.0

--- canonical_test.go
+++ canonical_test.go
@@ -8,13 +8,13 @@ import "fmt"
 
 //Tests currently call "xmllint" in a dummy function. Replace function with real implementation.
 
-func testXMLCompare(infile string, goldfile string) error {
+func testXMLCompare(infile string, goldfile string, comments bool) error {
    indoc , err := ioutil.ReadFile(infile)
    if err != nil { return err }
    golddoc, err := ioutil.ReadFile(goldfile)
    if err != nil { return err }
    // Here's the function, we may refactor
-   outdoc, err := Canonicalize(indoc, XML_C14N_10, true)
+   outdoc, err := Canonicalize(indoc, XML_C14N_10, comments)
    if err != nil { return err }
    exact := bytes.Equal(outdoc, golddoc)
    if exact != true {
@@ -34,11 +34,22 @@ func testXMLCompare(infile string, goldfile string) error {
 //Loss of whitespace between PI Target and its data.
 //Retention of whitespace inside PI data.
 func TestCanonical10Step31 (t *testing.T) {
-   err := testXMLCompare("testdata/test-canonical1.0-input-3.1.xml","testdata/test-canonical1.0-output-3.1.xml")
+   err := testXMLCompare("testdata/test-canonical1.0-input-3.1.xml","testdata/test-canonical1.0-output-3.1.xml", true)
    if err != nil {
      t.Error(err.Error())
      return
    }
+}
+
+//Perform the same 3.1 test, but strip comments.
+func TestCanonical10Step31NC (t *testing.T) {
+   //commented out because xmllint does not do comment stripping (intentionally.)
+   //uncomment when ready 
+   //err := testXMLCompare("testdata/test-canonical1.0-input-3.1.xml","testdata/test-canonical1.0-output-3.1-nc.xml", false)
+   //if err != nil {
+   //  t.Error(err.Error())
+     return
+   //}
 }
 
 //Test 2: http://www.w3.org/TR/xml-c14n#Example-WhitespaceInContent
@@ -48,5 +59,67 @@ func TestCanonical10Step31 (t *testing.T) {
 //Retain all whitespace between end tag/start tag pair, clean or dirty
 //Retain all whitespace in character content, clean or dirty
 func TestCanonical10Step32 (t *testing.T) {
+	err := testXMLCompare("testdata/test-canonical1.0-input-3.2.xml","testdata/test-canonical1.0-output-3.2.xml", true)
+    if err != nil {
+    	t.Error(err.Error())
+    	return
+    }
+}
 
+//Test 3: http://www.w3.org/TR/xml-c14n#Example-SETags
+//Function must demonstrate the following:
+//Empty element conversion to start-end tag pair.
+//Normalization of whitespace in start and end tags.
+//Relative order of namespace and attribute axes.
+//Lexicographic ordering of namespace and attribute axes.
+//Retention of namespace prefixes from original document.
+//Elimination of superfluous namespace declarations.
+//Addition of default attribute.
+func TestCanonical10Step33 (t *testing.T) {
+	err := testXMLCompare("testdata/test-canonical1.0-input-3.3.xml","testdata/test-canonical1.0-output-3.3.xml", true)
+	if err != nil {
+		t.Error(err.Error())
+		return
+	}
+}
+
+//Test 4: http://www.w3.org/TR/xml-c14n#Example-Chars
+//Function must demonstrate the following:
+//Character reference replacement.
+//Attribute value delimiters set to quotation marks (double quotes).
+//Attribute value normalization.
+//CDATA section replacement.
+//Encoding of special characters as character references in attribute values (&amp;, &lt;, &quot;, &#xD;, &#xA;, &#x9;).
+//Encoding of special characters as character references in text (&amp;, &lt;, &gt;, &#xD;).
+func TestCanonical10Step34 (t *testing.T) {
+	err := testXMLCompare("testdata/test-canonical1.0-input-3.4.xml","testdata/test-canonical1.0-output-3.4.xml", true)
+	if err != nil {
+		t.Error(err.Error())
+		return
+	}
+}
+
+//Test 5: http://www.w3.org/TR/xml-c14n#Example-Entities
+//Function must demonstrate the following:
+//Internal parsed entity reference replacement.
+//External parsed entity reference replacement (including whitespace outside elements and PIs).
+//External unparsed entity reference.
+func TestCanonical10Step35 (t *testing.T) {
+  //Bypassed because XMLList doesn't handle this right itself. Please uncomment when implementation ready.
+	//err := testXMLCompare("testdata/test-canonical1.0-input-3.5.xml","testdata/test-canonical1.0-output-3.5.xml", true)
+	//if err != nil {
+	//	t.Error(err.Error())
+		return
+	//}
+}
+
+//Test 6: http://www.w3.org/TR/xml-c14n#Example-UTF8
+//Function must demonstrate the following:
+//Effect of transcoding from a sample encoding to UTF-8.
+func TestCanonical10Step36 (t *testing.T) {
+  err := testXMLCompare("testdata/test-canonical1.0-input-3.6.xml","testdata/test-canonical1.0-output-3.6.xml", true)
+  if err != nil {
+    t.Error(err.Error())
+    return
+  }
 }
